fdea92ab12e9e35130da7d053a5cb9cce2823802
Igor Henrique da Silva <96557071+Igor-Yngvi@users.noreply.github.com>
2024-04-09 23:56:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ document.querySelectorAll('.add-to-backpack').forEach(button => {
   button.addEventListener('click', () => {
     const card = button.parentElement;
     const itemName = card.querySelector('h3').textContent;
+    const itemImageSrc = card.querySelector('img').src; // Obtemos o SRC da imagem
     const listItem = document.createElement('li');
-    listItem.textContent = itemName;
+    listItem.innerHTML = `<img src="${itemImageSrc}" alt="${itemName}" class="item-image"> ${itemName}`; // Adicionamos a imagem pequena junto ao nome do item
     document.querySelector('.backpack ul').appendChild(listItem);
   });
 });
@@ -14,4 +15,16 @@ document.getElementById('show-backpack').addEventListener('click', () => {
 
 document.getElementById('close-backpack').addEventListener('click', () => {
   document.querySelector('.backpack').classList.add('hidden');
+});
+
+// seleciona o elemento pai
+var parent = document.querySelector('ul');
+
+// adiciona um evento de clique ao elemento pai
+parent.addEventListener('click', function(e) {
+  // verifica se o elemento clicado é um 'li'
+  if (e.target && e.target.nodeName == 'LI') {
+    // remove o elemento li quando ele é clicado
+    e.target.remove();
+  }
 });
